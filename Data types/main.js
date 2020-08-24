fc1a2473d8c10f471eb43d70e782d32bebac8174
tragislav <stashockey2000@gmail.com>
2020-08-24 17:37:47 +0300

    fix

--- Data types/main.js
+++ Data types/main.js
@@ -8,10 +8,10 @@ alert( `Привет, ${name}!` ); // Привет, Иван!
 // Вставим выражение
 alert( `результат: ${1 + 2}` ); // результат: 3
 
-let name = "Ilya";
+name = "Ilya";
 
 alert( `hello ${1}` ); // hello 1
 
-alert( `hello ${"name"}` ); // hello name
+alert( `hello ${"names"}` ); // hello name
 
 alert( `hello ${name}` ); // hello Ilya
