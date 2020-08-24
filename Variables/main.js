29afa6995a7707b08bffb74ab48aa1e871df80ce
tragislav <stashockey2000@gmail.com>
2020-08-24 16:50:35 +0300

    variables

---
+++ Variables/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let name = 'John';
+let admin;
+let ourPlanetName = 'Земля';
+let currentUserName = 'Джон';
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+
+admin = name;
+
+alert(admin);
