4bb23538e8aca492e8624a235273ba2d0f24c8e2
tragislav <stashockey2000@gmail.com>
2020-08-24 17:58:24 +0300

    Type conversion

--- Variables/main.js
+++ Variables/main.js
@@ -4,8 +4,8 @@ let name = 'John';
 let admin;
 let ourPlanetName = 'Земля';
 let currentUserName = 'Джон';
-const BIRTHDAY = '18.04.1982';
-const age = someCode(BIRTHDAY);
+// const BIRTHDAY = '18.04.1982';
+// const age = someCode(BIRTHDAY);
 
 admin = name;
 
