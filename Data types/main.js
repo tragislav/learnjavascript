c8b6cb81df5c6bcae57b946ebfb4db7a71abe466
tragislav <stashockey2000@gmail.com>
2020-08-24 17:32:27 +0300

    Data types

---
+++ Data types/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let name = "Иван";
+
+// Вставим переменную
+alert( `Привет, ${name}!` ); // Привет, Иван!
+
+// Вставим выражение
+alert( `результат: ${1 + 2}` ); // результат: 3
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
