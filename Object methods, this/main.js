863a86bad3339b60438d66fc13b5ee1f264341b0
tragislav <stashockey2000@gmail.com>
2020-09-07 19:53:52 +0300

    Object methods, this

---
+++ Object methods, this/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+//Калькулятор
+let calculator = {
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    },
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    },
+};
+  
+calculator.read();
+alert( `Сумма a и b равна ${ calculator.sum() } `);
+alert(`Произведение a и b равно ${ calculator.mul() } `);
+
+//Цепь вызовов
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+      alert( this.step );
+      return this;
+    }
+};
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); 
+//OR
+ladder.up().up().down().showStep(); 
+
